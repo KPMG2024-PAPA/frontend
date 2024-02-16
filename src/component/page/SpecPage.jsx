a7140d990173988fceefc6bd3a03a225a30a9ad8
zissuuo <zissuuo@yonsei.ac.kr>
2024-02-17 02:42:15 +0900

    feat: add specification guideline

--- src/component/page/SpecPage.jsx
+++ src/component/page/SpecPage.jsx
@@ -5,12 +5,12 @@ import { createGlobalStyle, keyframes } from 'styled-components';
 import { useState, useEffect } from 'react';
 import { CopyToClipboard } from 'react-copy-to-clipboard';
 
-
 import ImgUpload from '../ui/ImgUpload';
 import TextInput from '../ui/TextInput';
 import Button from '../ui/Button';
 import HeaderComponent from '../ui/HeaderComponent';
 import { animationMixin } from '../effect/Animation';
+import Dialog from '../ui/Dialog';
 
 const AllGlobalStyle = createGlobalStyle`
   @font-face {
@@ -158,6 +158,27 @@ const CustomButton1 = styled(Button)`
   margin-top: 20px;
 `;
 
+const GuideButton = styled(Button)`
+  background-color: #252a2f;
+  //height: auto;
+  box-shadow: none;
+  width: fit-content;
+  //margin-top: 20px;
+  font-size: 16px;
+  box-shadow: inset 0px 0px 3px rgba(0, 0, 0, 0.1);
+  padding:none;
+  font-family: 'Pretendard-ExtraBold';
+`;
+
+const GuideContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-direction: row;
+  width: 100%;
+`;
+
+
 const ButtonContainer2 = styled.div`
   display: flex;
   justify-content: center;
@@ -202,7 +223,7 @@ const CustomButton2 = styled(Button)`
   width: 25px;
   height: fit-content;
   margin-top: 20px;
-  font-size: 30px;
+  font-size: 23px;
 `;
 
 
@@ -271,7 +292,9 @@ const SpecPage = () => {
   const [text, setText] = useState('');
   const [selectedFile, setSelectedFile] = useState(null);
   const [responseData, setResponseData] = useState('');
-  
+  const [modalVisible, setModalVisible] = useState(false);
+
+
   const Tooltip = ({children, message})=> {
     return (
       <ButtonContainer2>
@@ -281,6 +304,10 @@ const SpecPage = () => {
     );
   }
 
+  const handleGuideButtonClick = () => {
+    setModalVisible(true);
+  };
+  
 
   const handleChange = (event) => {
     // 사용자 입력에 따라 텍스트를 업데이트합니다.
@@ -390,7 +417,7 @@ const SpecPage = () => {
           <ThirdWrapper>
             <GuideText> ☝🏻 도면을 업로드해주세요 </GuideText>
             <ImgUpload onFileSelect={handleFileSelect} />
-            <GuideText> ✌🏻 발명품에 대한 설명을 해주세요 </GuideText>
+            <GuideText> ✌🏻 발명에 대한 설명을 해주세요 </GuideText>
             <CustomTextInput
               placeholder="텍스트를 입력해주세요"
               value={text}
@@ -402,9 +429,12 @@ const SpecPage = () => {
             </ButtonContainer1>
           </ThirdWrapper>
           <FourthWrapper>
-            <GuideText>📜 PAPA가 작성한 초안이에요</GuideText>
+            <GuideContainer>
+              <GuideText>📜 PAPA가 작성한 명세서 초안이에요</GuideText>
+              <GuideButton title='💡 Guideline' onClick={handleGuideButtonClick} />
+            </GuideContainer>
             <Box>
-              <BoxText>{responseData || "작성한 명세서가 여기에 작성됩니다"}</BoxText>
+              <BoxText>{responseData || "명세서 초안이 여기에 작성됩니다"}</BoxText>
             </Box>
             <ButtonContainer2>
               <Tooltip message="Copy">
@@ -412,13 +442,49 @@ const SpecPage = () => {
                 <CustomButton2 title='📋' /></CopyToClipboard> </Tooltip>
               <Tooltip message="Rewrite"> 
                 <CustomButton2 title='🔃' onClick={handleButtonClick}/> </Tooltip>
-              <Tooltip message="안내 메세지"> 
-                <CustomButton2 title='💡'/> </Tooltip>
             </ButtonContainer2>
           </FourthWrapper>
         </SecondWrapper>
       </Wrapper>
       {isLoading && <LoadingOverlay />}
+      {modalVisible && (
+        <Dialog
+          title="모범 명세서 작성 가이드라인"
+          onConfirm={() => setModalVisible(false)}
+          visible={modalVisible}
+          confirmText="닫기"
+        >
+          <p style={{ fontSize: '20px', fontFamily: 'Pretendard-ExtraBold' }}>발명의 명칭</p>
+          <p>[발명의 명칭] 란에는 그 발명의 내용을 간명하게 표시할 수 있는 발명의 명칭을 기재하여야 하며, 영문 명칭을 &#123;&#125;안에 다음 예와 같이 입력하여야 합니다.</p>
+          <p>[예] [발명의 명칭] 자동차용 범퍼&#123;AUTOMOBILE BUMPER&#125;</p>
+          <p style={{ fontSize: '20px', fontFamily: 'Pretendard-ExtraBold' }}>발명의 상세한 설명</p>
+          <p>[발명의 상세한 설명] 란은 원칙적으로[기술분야], [배경기술], [발명(고안)의 내용], [발명(고안)의 실시를 위한 구체적인 내용] 및 ([산업상 이용가능성])란으로 구분하여 기재하며, 그 내용은 그 발명이 속하는 기술분야에서 통상의 지식을 가진 자가 그 발명을 쉽게 이해하고 또한 쉽게 실시할 수 있도록 「특허법」제42조제3항 및 「특허법 시행규칙」 제21조제3항에 따라 명확하고 상세하게 기재합니다.</p>
+          <p style={{ marginLeft: '15px' }}>﹒[기술분야] 란에는 특허(실용신안등록)를 받고자 하는 발명(고안)의 기술분야를 명확하고 간결하게 기재합니다.</p>
+          <p style={{ marginLeft: '30px' }}>﹒[예] 본 발명은 ㆍㆍㆍ하기 위한 ㆍㆍㆍ에 관한 ㆍㆍㆍㆍㆍㆍ.</p>
+          <p style={{ marginLeft: '15px' }}>﹒[배경기술] 란에는 발명(고안)의 이해, 조사 및 심사에 유용하다고 생각되는 종래의 기술을 명시하고, 특허(실용신안등록)를 받고자 하는 자가 종래기술의 문헌 정보를 알고 있는 때에는 그 문헌의 명칭, 발간일, 종래기술이 기재된 페이지 등의 정보를 기재합니다.</p>
+          <p style={{ marginLeft: '15px' }}>﹒[발명(고안)의 내용] 란은 원칙적으로 [해결하고자 하는 과제], [과제의 해결 수단] 및 [효과] 란으로 구분하여 다음과 같이 기재합니다. 다만, 구분하여 기재하기 어려운 경우에는 별도로 나누어 기재하지 않아도 됩니다.</p>
+          <p style={{ marginLeft: '30px' }}>(1) [해결하고자 하는 과제] 란에는 특허(실용신안등록)를 받고자 하는 발명(고안)이 과제로 하고 있는 종래 기술의 문제점 등을 기재합니다.</p>
+          <p style={{ marginLeft: '30px' }}>(2) [과제의 해결 수단] 란에는 특허(실용신안등록)를 받고자 하는 발명(고안)에 의하여 어떻게 해당 과제가 해결되었는지를 기재합니다. 일반적으로는 청구항에 기재된 발명(고안) 그 자체가 해결수단이 되므로 청구항에 기재된 발명(고안)을 기재하면 됩니다.</p>
+          <p style={{ marginLeft: '30px' }}>(3) [효과] 란에는 특허(실용신안등록)를 받고자 하는 발명(고안)이 종래의 기술과 비교하여 우수하다고 인정되는 사항을 기재합니다.</p>
+          <p style={{ marginLeft: '15px'}}>﹒[발명(고안)을 실시하기 위한 구체적인 내용] 란에는 그 발명(고안)이 속하는 기술분야에서 통상의 지식을 가진 자가 그 발명(고안)이 어떻게 실시되는지를 쉽게 알 수 있도록 그 발명(고안)의 실시를 위한 구체적인 내용을 적어도 하나 이상, 가급적 여러 형태로 기재합니다. 필요한 경우에는[실시예] 란을 만들어 기재하고, 도면이 있으면 그 도면을 인용하여 기재합니다.</p>
+          <p style={{ marginLeft: '15px'}}>﹒[산업상 이용가능성] 란은 특허(실용신안등록)를 받고자 하는 발명(고안)이 산업상 이용가능한 것인지 여부가 불분명할 때 그 발명(고안)의 산업상 이용방법, 생산방법 또는 사용방법 등을 기재합니다. 대부분의 경우 산업상 이용가능성은 명세서의 다른 기재 사항으로부터 충분히 유추가 가능하므로 별도의 기재는 필요하지 않습니다.</p>
+          <p style={{ fontSize: '20px', fontFamily: 'Pretendard-ExtraBold' }}>특허청구범위</p>
+          <p>[특허청구범위] 란은 다음과 같이 기재합니다.</p>
+          <p style={{ marginLeft: '15px'}}>﹒[특허(실용신안등록)청구범위] 란의 [청구항] 란은 독립청구항(이하 “독립항”이라 함)을 기재하며, 그 독립항을 한정하거나 부가하여 구체화하는 종속청구항(이하 “종속항”이라 함)을 기재할 수 있습니다. 이 경우 필요한 때에는 그 종속항을 한정하거나 부가하여 구체화하는 다른 종속항을 기재할 수 있습니다.</p>
+          <p style={{ marginLeft: '15px'}}>﹒청구항은 발명(고안)의 성질에 따라 적정한 수로 기재하여야 합니다.</p>
+          <p style={{ marginLeft: '15px'}}>﹒종속항을 기재할 때에는 독립항 또는 다른 종속항 중에서 1 또는 2 이상의 항을 인용하여야 하며, 인용되는 항의 번호를 기재하여야 합니다.</p>
+          <p style={{ marginLeft: '15px'}}>﹒2 이상의 항을 인용하는 청구항은 인용되는 항의 번호를 택일적으로 기재하여야 합니다.</p>
+          <p style={{ marginLeft: '15px'}}>﹒2 이상의 항을 인용한 청구항에서 그 청구항의 인용된 항은 다시 2 이상의 항을 인용하는 방식을 사용하여서는 아니됩니다. 2 이상의 항을 인용한 청구항에서 그 청구항의 인용된 항이 다시 하나의 항을 인용한 후에 그 하나의 항이 결과적으로 2 이상의 항을 인용하는 방식에 대하여도 또한 같습니다.</p>
+          <p style={{ marginLeft: '15px'}}>﹒인용되는 청구항은 인용하는 청구항보다 먼저 기재하여야 합니다.</p>
+          <p style={{ marginLeft: '15px'}}>﹒각 청구항은 항마다 행을 바꾸어 기재하고, 그 기재하는 순서에 따라 아라비아숫자로 일련번호를 붙여야 합니다.</p>
+          <p style={{ marginLeft: '15px'}}>﹒발명(고안)의 기술적 특징을 이해하기 위하여 필요한 경우에는 도면의 인용부호를 특허(실용신안등록)청구범위에 기재할 수 있습니다.</p>
+          <p style={{ marginLeft: '15px'}}>﹒「특허법」제42조제5항 본문(「실용신안법」제8조제5항 본문)에 따라 특허(실용신안등록)청구범위를 기재하지 않는 경우에는 [특허(실용신안등록)청구범위] 및 [청구항] 식별항목을 삭제합니다.</p>
+          <p style={{ fontSize: '20px', fontFamily: 'Pretendard-ExtraBold' }}>도면의 간단한 설명</p>
+          <p>[도면의 간단한 설명] 란에는 첨부한 ‘도면’ 각각에 대하여 각 ‘도면’이 무엇을 표시한 것인가를 간단히 기재합니다. 다만, 발명의 설명에 ‘도면’이 필요하지 아니한 경우에는 [도면의 간단한 설명] 식별항목을 삭제하여야 합니다.</p>
+          <p>[예] 제1도는 전체를 조립한 평명도, 제2도는 어느 부분을 보인 정면도, 제3도는 어느 부분의 종단면도 등</p>
+        </Dialog>
+      )}
+
     </div>
   );
 };
